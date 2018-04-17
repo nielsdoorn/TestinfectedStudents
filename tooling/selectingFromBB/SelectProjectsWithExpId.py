f26c1c38af9c534d113e3afa2a7577a96e9613dd
nielsdoorn <nielsdoorn@gmail.com>
2018-04-17 14:28:21 +0200

    changed back to params

--- tooling/selectingFromBB/SelectProjectsWithExpId.py
+++ tooling/selectingFromBB/SelectProjectsWithExpId.py
@@ -18,8 +18,8 @@ class SelectProjectsWithExpId:
                     INNER JOIN sessions_for_experiment s ON m.session_id=s.id 
                     INNER JOIN sessions ses ON ses.id=m.session_id 
                     AND project_id IS NOT NULL
-                    WHERE ses.created_at BETWEEN '2017-08-28 00:00:00' AND '2017-11-13 00:00:00'"""
-        self.cursor.execute(sql, self.expId)
+                    WHERE ses.created_at BETWEEN '%s' AND '%s'"""
+        self.cursor.execute(sql, (self.expId, self.start, self.end))
         projectsResult = self.cursor.fetchall()
         if len(projectsResult) > 0:
             for projectResult in projectsResult:
