a531724df610791ee4ebca0af4fe92139208cba8
nielsdoorn <nielsdoorn@gmail.com>
2018-04-17 14:34:32 +0200

    debugging info

--- tooling/selectingFromBB/SelectProjectsWithExpId.py
+++ tooling/selectingFromBB/SelectProjectsWithExpId.py
@@ -19,6 +19,7 @@ class SelectProjectsWithExpId:
                     INNER JOIN sessions ses ON ses.id=m.session_id 
                     AND project_id IS NOT NULL
                     WHERE ses.created_at BETWEEN %s AND %s"""
+        print(sql % (self.expId, self.start, self.end))
         self.cursor.execute(sql, (self.expId, self.start, self.end))
         projectsResult = self.cursor.fetchall()
         if len(projectsResult) > 0:
