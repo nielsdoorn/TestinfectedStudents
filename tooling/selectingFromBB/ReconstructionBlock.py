ecc49abbf3d2e4d210827a30cdf566e612d62bc5
nielsdoorn <nielsdoorn@gmail.com>
2018-04-22 10:10:59 +0200

    oops

--- tooling/selectingFromBB/ReconstructionBlock.py
+++ tooling/selectingFromBB/ReconstructionBlock.py
@@ -20,15 +20,15 @@ class ReconstructionBlock:
         for f in project.files:
             if len(f.name.split('/')) > 1:
                 theDir = "output/%s/%s/%s" % (self.name, project.projectId, f.name[:f.name.rfind('/')])
-		try:
-            os.stat(theDir)
-		except:
-    		os.makedirs(theDir)   
-            print("[CON]  making subdirs %s" % theDir)
+		        try:
+                    os.stat(theDir)
+		        except:
+    		        os.makedirs(theDir)   
+                    print("[CON]  making subdirs %s" % theDir)
             
-        with codecs.open("output/%s/%s/%s" % (self.name, project.projectId, f.name), "w+", encoding="utf-8") as output:
-            print("[CON] call print compile output with: src: %s mastereventid: %s" % (str(f.sourceFileId), str(f.masterEventId)))
-            call(["/tools/nccb/bin/print-compile-input", "/data/compile-inputs", str(f.sourceFileId), str(f.masterEventId)], stdout=output)            
+            with codecs.open("output/%s/%s/%s" % (self.name, project.projectId, f.name), "w+", encoding="utf-8") as output:
+                print("[CON] call print compile output with: src: %s mastereventid: %s" % (str(f.sourceFileId), str(f.masterEventId)))
+                call(["/tools/nccb/bin/print-compile-input", "/data/compile-inputs", str(f.sourceFileId), str(f.masterEventId)], stdout=output)            
 		
         if self.nextBlock is not None:
             self.nextBlock.process(project)
